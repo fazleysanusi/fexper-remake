972fb2bdc425dbc911e64145e5110d7b7f910c34
FazleyPali <fazleysanusi8@gmail.com>
2017-12-10 01:53:40 +0800

    push

--- login/db.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  // Connecting, selecting database
-  $dbconn = pg_connect("host=127.0.0.1 dbname=postgres user=postgres password=fexper")
-  or die('Could not connect: ' . pg_last_error());
-  $con = $dbconn;
-
-?>
